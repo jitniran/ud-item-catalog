67ddf6f79356c6effe9eeb42cdceb0008eba3f92
jitniran <jitshakes@gmail.com>
2017-12-01 13:30:10 +0530

    added create and show for sport catalog

--- views/category.py
+++ views/category.py
@@ -1,17 +1,32 @@
 from flask import Blueprint, render_template
+from flask import url_for, request, redirect
 from .initdb import session
+from models.model import Sport, SportItem
 
-category = Blueprint('category', __name__,
-                    template_folder='templates/catalog')
+category = Blueprint('category', __name__)
+
 
 @category.route('/')
-def root():
-    return 'main page'
-
 @category.route('/catalog')
 def catalog():
-    return 'hello'
+    """
+    method that lists all sports catagories
+    """
+    sport_catalog = session.query(Sport)
+    return render_template('sports/catalog.html', catalog=sport_catalog)
 
-@category.route('/catalog/new')
+
+@category.route('/catalog/new', methods=['GET', 'POST'])
 def new():
-    return render_template('new')
+    """
+    method for creating new sport
+    """
+    if(request.method == "POST"):
+        name = request.form['name']
+        sport = Sport()
+        sport.name = name
+        session.add(sport)
+        session.commit()
+        return redirect(url_for('category.catalog'))
+    else:
+        return render_template('sports/new.html')
--- views/initdb.py
+++ views/initdb.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.model import Base, Sport, SportItem
+from models.model import Base
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///sportitems.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
